b9d95d2f8a731d315ed76d5faee90edc882bcfdf
Greatwolf <github.greatwolf@mamber.net>
2020-04-05 17:18:37 -0700

    Generate html page map for coordinates and open in browser.

--- geolocate.lua
+++ geolocate.lua
@@ -20,6 +20,7 @@
 local dump = require 'pl.pretty'.dump
 local json = require 'dkjson'
 local https = require 'ssl.https'
+local luacom = require 'luacom'
 
 local url = "https://www.googleapis.com/geolocation/v1/geolocate"
 local api_key = "AIzaSyBOti4mM-6x9WDnZIjIeyEU21OpBXqWBgw" -- google's chrome apikey
@@ -41,6 +42,39 @@ local function geomac_fromfile (filename)
   return result
 end
 
+local pagetemplate =
+[[<!DOCTYPE html>
+<title>Geolocation Map</title>
+<meta charset="utf-8">
+<style>
+#mapcanvas
+{
+  width:800px;
+  height:600px;
+  border:0;
+}
+</style>
+<section>
+  <h1>Geolocation Map</h1>
+  <article>
+    <h2>Your location:</h2>
+    <p>
+    <iframe id="mapcanvas"
+      src="https://maps.google.com/maps?q=%f,%f&output=embed">
+    </iframe>
+    </p>
+  </article>
+</section>
+]]
+local function make_geopage(filename, lat, long)
+  local geofile = assert (io.open(filename, 'w'))
+  geofile:write(pagetemplate:format(lat, long))
+end
+
+local function sh(cmd)
+  luacom.CreateObject "wscript.shell":run(cmd, 1, false)
+end
+
 local resp = {}
 for i, macFile in ipairs (arg) do
   local post_data = json.encode {wifiAccessPoints = geomac_fromfile (macFile)}
@@ -55,6 +89,10 @@ for i, macFile in ipairs (arg) do
   }
   resp = assert (json.decode (table.concat (resp)))
 
+  macFile = macFile == "-" and "stdin" or macFile
   print (string.format ("[%d] %s:", i, macFile))
   dump (resp)
+  local filename = "geolocate_" .. macFile .. ".html"
+  make_geopage (filename, resp.location.lat, resp.location.lng)
+  sh(filename)
 end
